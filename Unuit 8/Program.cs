420ddbc8b4efebfa977804a4143f179618b78626
SDozH <soldatov_alex@bk.ru>
2022-07-15 08:38:04 +0300

    папки диска с и файлы в корне

--- Unuit 8/Program.cs
+++ Unuit 8/Program.cs
@@ -10,20 +10,26 @@ namespace Unuit_8
         {
             static void Main(string[] args)
             {
-                // получим системные диски
-                DriveInfo[] drives = DriveInfo.GetDrives();
+                GetCatalogs(); //   Вызов метода получения директорий
+            }
 
-                // Пробежимся по дискам и выведем их свойства
-                foreach (DriveInfo drive in drives)
+            static void GetCatalogs()
+            {
+                string dirName = @"/"; // Прописываем путь к корневой директории MacOS (для Windows скорее всего тут будет "C:\\")
+                if (Directory.Exists(dirName)) // Проверим, что директория существует
                 {
-                    Console.WriteLine($"Название: {drive.Name}");
-                    Console.WriteLine($"Тип: {drive.DriveType}");
-                    if (drive.IsReady)
-                    {
-                        Console.WriteLine($"Объем: {drive.TotalSize}");
-                        Console.WriteLine($"Свободно: {drive.TotalFreeSpace}");
-                        Console.WriteLine($"Метка: {drive.VolumeLabel}");
-                    }
+                    Console.WriteLine("Папки:");
+                    string[] dirs = Directory.GetDirectories(dirName);  // Получим все директории корневого каталога
+
+                    foreach (string d in dirs) // Выведем их все
+                        Console.WriteLine(d);
+
+                    Console.WriteLine();
+                    Console.WriteLine("Файлы:");
+                    string[] files = Directory.GetFiles(dirName);// Получим все файлы корневого каталога
+
+                    foreach (string s in files)   // Выведем их все
+                        Console.WriteLine(s);
                 }
             }
         }
