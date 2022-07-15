cc0b867af36230c623f922f6f2a8701728d11ea9
SDozH <soldatov_alex@bk.ru>
2022-07-15 23:51:32 +0300

    8/4/2правильный

--- Unuit 8/Program.cs
+++ Unuit 8/Program.cs
@@ -5,19 +5,36 @@ namespace Unuit_8
     using System;
     using System.IO;
     class Task
-    {
-        public static void Main()
+    { 
+        class Program
         {
-            var fileInfo = new FileInfo("/Users/Dzh/Desktop/BinaryFile.bin"); // Укажем путь
+            public static void Main()
+            {
+                WriteValues();
+                ReadValues();
+            }
 
-                // Откроем файл и прочитаем его содержимое
-                using (StreamReader sr = fileInfo.OpenText())
+            static void WriteValues()
+            {
+                using (BinaryWriter writer = new BinaryWriter(File.Open("/Users/Dzh/Desktop/BinaryFile.bin", FileMode.Open)))
+                    writer.Write($"Файл изменен {DateTime.Now} на компьютере c ОС {Environment.OSVersion}");
+            }
+
+            static void ReadValues()
+            {
+                string StringValue;
+
+                if (File.Exists("/Users/Dzh/Desktop/BinaryFile.bin"))
                 {
-                    string str = "";
-                    while ((str = sr.ReadLine()) != null)
-                        Console.WriteLine(str);
+                    using (BinaryReader reader = new BinaryReader(File.Open("/Users/Dzh/Desktop/BinaryFile.bin", FileMode.Open)))
+                    {
+                        StringValue = reader.ReadString();
+                    }
+
+                    Console.WriteLine(StringValue);
                 }
             }
         }
     }
+}
 
