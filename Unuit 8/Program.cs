281bba285aafe59db637df5473890097f88fc672
SDozH <soldatov_alex@bk.ru>
2022-07-16 00:01:58 +0300

    8/4/3

--- Unuit 8/Program.cs
+++ Unuit 8/Program.cs
@@ -4,35 +4,40 @@ namespace Unuit_8
 {
     using System;
     using System.IO;
-    class Task
-    { 
+    using System.Runtime.Serialization.Formatters.Binary;
+    namespace Serialization
+    {
+
+        // Описываем наш класс и помечаем его атрибутом для последующей сериализации   
+        [Serializable]
+        class Contact
+        {
+            public string Name { get; set; }
+            public long PhoneNumber { get; set; }
+            public string Email{get; set;}
+            public Contact(string name, long phoneNumber, string email)
+            {
+                Name = name;
+                PhoneNumber = phoneNumber;
+                Email = email;
+            }
+        }
         class Program
         {
-            public static void Main()
+            static void Main(string[] args)
             {
-                WriteValues();
-                ReadValues();
-            }
+                // объект для сериализации
+                var person = new Contact("Alex",9253237979, "Soldatov_alex@bk.ru");
+                Console.WriteLine("Объект создан");
 
-            static void WriteValues()
-            {
-                using (BinaryWriter writer = new BinaryWriter(File.Open("/Users/Dzh/Desktop/BinaryFile.bin", FileMode.Open)))
-                    writer.Write($"Файл изменен {DateTime.Now} на компьютере c ОС {Environment.OSVersion}");
-            }
-
-            static void ReadValues()
-            {
-                string StringValue;
-
-                if (File.Exists("/Users/Dzh/Desktop/BinaryFile.bin"))
+                BinaryFormatter formatter = new BinaryFormatter();
+                // получаем поток, куда будем записывать сериализованный объект
+                using (var fs = new FileStream("/Users/Dzh/Desktop/Contact1.bin", FileMode.OpenOrCreate))
                 {
-                    using (BinaryReader reader = new BinaryReader(File.Open("/Users/Dzh/Desktop/BinaryFile.bin", FileMode.Open)))
-                    {
-                        StringValue = reader.ReadString();
-                    }
-
-                    Console.WriteLine(StringValue);
+                    formatter.Serialize(fs, person);
+                    Console.WriteLine("Объект сериализован");
                 }
+      
             }
         }
     }
