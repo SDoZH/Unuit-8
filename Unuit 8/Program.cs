15ab84b2d35ec3643d9fbc86dccfbe698c60aab8
SDozH <soldatov_alex@bk.ru>
2022-07-15 15:30:11 +0300

    8/3/2

--- Unuit 8/Program.cs
+++ Unuit 8/Program.cs
@@ -4,18 +4,24 @@ namespace Unuit_8
 {
     using System;
     using System.IO;
-    class FileWriter
+    class Task
     {
         public static void Main()
         {
-            string filePath = @"\Users\Dzh\source\repos\Unuit 8\Unuit 8\Program.cs"; // Укажем путь
-            // Откроем файл и прочитаем его содержимое
-            using (StreamReader sr = File.OpenText(filePath))
+            var fileInfo = new FileInfo("/Users/Dzh/source/repos/Unuit 8/Unuit 8/Program.cs"); // Укажем путь
+
+            using (StreamWriter sw = fileInfo.AppendText())
             {
-                string str = "";
-                while ((str = sr.ReadLine()) != null)
-                    Console.WriteLine(str);
+                sw.WriteLine($"Время запуска: {DateTime.Now}");
+            }
+                // Откроем файл и прочитаем его содержимое
+                using (StreamReader sr = fileInfo.OpenText())
+                {
+                    string str = "";
+                    while ((str = sr.ReadLine()) != null)
+                        Console.WriteLine(str);
+                }
             }
         }
     }
-}
+
